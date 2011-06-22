149d45c38fb2fcb1ff911f5a09f635aded2076ad
Janak Chandarana <jac099@yahoo.com>
2011-06-22 17:52:40 +0530

    lib heap added

--- src/setup.sh
+++ src/setup.sh
@@ -1,14 +1,15 @@
 #create the ll object file first
 gcc -fPIC -g -c -Wall src/ll.c
+gcc -fPIC -g -c -Wall src/heap.c
 
 #Now create shared object (library)
 gcc -shared -o lib/liblinkedlist.so ll.o 
+gcc -shared -o lib/libheap.so heap.o 
 
 #compile any code and link with library
-gcc src/llcaller.c -o bin/llcaller -L/scratch/flicktest/src/lib/ -llinkedlist
+gcc src/llcaller.c -o bin/llcaller -L`pwd`/lib/ -llinkedlist
+gcc src/heapcaller.c -o bin/heapcaller -L`pwd`/lib/ -lheap
 
 #Dont forget to keep LIB path in env variable 
-export LD_LIBRARY_PATH=/scratch/flicktest/src/lib/:${LD_LIBRARY_PATH}
-
-
+export LD_LIBRARY_PATH=/scratch/repos/flicktest/src/lib/
 
--- src/src/heap.c
+++ src/src/heap.c
@@ -86,34 +86,4 @@ int minheap_traverse(minheap *hp)
   }
   return MINHEAP_SUCC;
 }
-int display(void *data1)
-{
-  printf("%d\n", *(int*)data1);
-  return MINHEAP_SUCC;
-}
-
-int compare(void *data1, void *data2)
-{
-  if (*(int*)data1 == *(int*)data2)
-    return 0;
-  else if(*(int*)data1 > *(int*)data2)
-    return 1;
-  else
-    return -1;
-}
-
-int main()
-{
-  int i = 0;
-  minheap *hp = NULL;
-
-  minheap_init(&hp, 50, compare, display);
-  for (i = 1; i < 20; i++)
-  {
-    minheap_add(hp, &i, sizeof(i));
-  }
-  minheap_traverse(hp);
-  return 0;
-}
-
 
