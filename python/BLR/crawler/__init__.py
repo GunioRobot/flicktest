143ea490a621130292aa2088109ae01a827dde2c
janak <jac099@yahoo.com>
2012-01-15 17:37:15 +0530

    BLRAMD changes

--- python/BLR/crawler/__init__.py
+++
@@ -1,9 +0,0 @@
-from crawler import yatracrawler
-
-def main():
-    print "hello 123"
-    yatracrawler.getpage("")
-
-# This is the standard boilerplate that calls the main() function.
-if __name__ == '__main__':
-    main()
